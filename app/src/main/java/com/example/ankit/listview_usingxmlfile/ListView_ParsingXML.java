393b63a2be5b6b1afd59a838fffb26c3e60dbc90
Pachouri-Sikandar <ankitpachouri0891@gmail.com>
2014-08-29 10:33:56 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ankit/listview_usingxmlfile/ListView_ParsingXML.java
@@ -0,0 +1,44 @@
+package com.example.ankit.listview_usingxmlfile;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class ListView_ParsingXML extends ListActivity {
+    ListView lv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       // setContentView(R.layout.activity_list_view__parsing_xml);
+
+        String[] listitem = getResources().getStringArray(R.array.Subjects_List);
+        this.setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, R.id.label, listitem));
+
+        lv = getListView();
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.list_view__parsing_xml, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
